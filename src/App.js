ee716a561406f352158f15047c107ab69a24d795
Jonas Busto <jonasrex00@gmail.com>
2022-11-07 23:50:58 -0300

    proyecton con bootstrap, router, fontawesome

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Components/Footer';
 import { Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash, } from '@fortawesome/free-solid-svg-icons'
+import "./Styles/app.css"
 
 const App = () => {
   return (
