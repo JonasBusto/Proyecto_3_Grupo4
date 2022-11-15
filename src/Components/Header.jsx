cc51ce461820a106e0cf93162db0834819abadb0
Mario Garcia <garciamd91@gmail.com>
2022-11-15 20:57:51 -0300

    creo header con sus componentes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from "react";
+import { Container , Row , Col , InputGroup , Form } from "react-bootstrap";
 import "../Styles/header.css";
 
 const Header = () => {
   return (
-    <>
-      <div
-        style={{ border: "1px black solid", width: "80%", marginTop: "1rem" }}
-      >
-        Header
-      </div>
-    </>
+    <Container fluid className="header-color header-heigh">
+      <Container>
+        <Row>
+          <Col>
+            <Container className="d-flex align-items-center justify-content-center">
+              <img src="/logo-RollingTravel.png" alt="" className="logo" />
+            </Container>
+          </Col>
+          <Col className="container-icon d-flex justify-content-around ">
+            <img src="/icon-twet.png" alt="" className="icon" />
+            <img src="/icon-face.png" alt="" className="icon" />
+            <img src="/icon-insta.png" alt="" className="icon" />
+          </Col>
+          <Col className="d-flex align-items-center justify-content-center">
+            <InputGroup className="py-2">
+              <Form.Control
+                placeholder="Buscar"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Text id="basic-addon2">Lupa</InputGroup.Text>
+            </InputGroup>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
