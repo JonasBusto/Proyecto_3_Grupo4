db9fedfc29a7ef9592971d81ef766ad9affcb2d4
Jonas Busto <jonasrex00@gmail.com>
2022-11-16 02:27:44 -0300

    primera version de home

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,25 +3,23 @@ import Hero from "../Components/Hero";
 import LikesPage from "../Components/LikesPage";
 import RecoverPass from "../Components/RecoverPass";
 import SearchResult from "../Components/SearchResult";
+import Home from "../Components/Home";
 import { Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import "../Styles/main.css";
 
-// ACÁ IRAN TODO EL CONTENIDO O TODOS LOS COMPONENTES DE PAGINA
-// EXCEPTO EL COMPONENTE DE NAV, HEADER Y FOOTER
 const Main = () => {
   return (
     <>
-      <div
-        style={{ border: "1px black solid", width: "80%", marginTop: "1rem" }}
-      >
-        Contenido Main
+      <main className="mx-auto" style={{ maxWidth: "1300px" }}>
+        {/* Contenido Main
         <Hero />
         <LikesPage />
         <RecoverPass />
-        <SearchResult />
-      </div>
+        <SearchResult /> */}
+        <Home />
+      </main>
     </>
   );
 };
