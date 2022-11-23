44c842aee363be6762ff00d45b82e1bde294f90e
Mario Garcia <garciamd91@gmail.com>
2022-11-23 00:33:52 -0300

    agrego link a logo footer y redes. Trabajo en img para que no se deformen

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import "../Styles/footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -19,27 +20,34 @@ const Footer = () => {
         <Container fluid>
           <Row className="d-flex">
             <Col
-              className="d-flex justify-content-center logo-footer"
+              className="d-flex pt-2 justify-content-center container-logo"
               xs={12}
               md={4}
               xl={4}
             >
-              <img
-                src="/logo-RollingTravel.png"
-                alt="Logo Rolling Travel"
-                width="30%"
-                height="auto"
-              />
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <img
+                  className="cursor-pointer-custom"
+                  alt=""
+                  src="/logo-RollingTravel.png"
+                />
+              </Link>
             </Col>
             <Col
-              className="d-flex justify-content-between icon-footer align-items-center"
+              className="d-flex pt-3 justify-content-around align-items-center container-icon-social"
               xs={12}
               md={4}
               xl={4}
             >
-              <img alt="" src="/icon-face-mini.png" width="10%" />
-              <img alt="" src="/icon-twet-mini.png" width="10%" />
-              <img alt="" src="/icon-insta-mini.png" width="10%" />
+              <a href="https://www.facebook.com/" target="blank">
+                <img alt="" src="/icon-face-mini.png" />
+              </a>
+              <a href="https://twitter.com/" target="blank">
+                <img alt="" src="/icon-twet-mini.png" />
+              </a>
+              <a href="https://www.instagram.com/" target="blank">
+                <img alt="" src="/icon-insta-mini.png" />
+              </a>
             </Col>
             <Col className="d-none d-md-block text-light p-5" md={4} xl={4}>
               <ul>
@@ -57,7 +65,7 @@ const Footer = () => {
           </Row>
           <Row>
             <Col>
-              <Card.Text className="text-center text-light copyright-footer">
+              <Card.Text className="text-center pt-4 text-light copyright-footer">
                 Copyright Rolling Travel S.A.
               </Card.Text>
             </Col>
