e42046d91072a80f751f8c4cbe92776607989eed
Mario Garcia <garciamd91@gmail.com>
2022-11-23 18:00:09 -0300

    creo maqueta de contacto

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,29 +1,54 @@
-import React from 'react'
-import { Container, Form, Row, Col } from 'react-bootstrap'
-import { Formik } from 'formik';
+import React from "react";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import { Formik } from "formik";
+import "../Styles/contact.css";
 
 const Contact = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Formik>
-            <Form>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  placeholder="Nombre/s"
-                  id="name"
-                  //  value={values.name}
-                />
-              </Form.Group>
-            </Form>
-          </Formik>
+    <Container fluid className="my-4">
+      <h2 className="text-center pb-2">Contactanos</h2>
+      <Row className="container-color m-0 mx-auto">
+        <Col md={6} className="pe-3">
+          <Form>
+            <Form.Group className="pb-3">
+              <Form.Control
+                type="text"
+                placeholder="Nombre/s"
+                id="name"
+                maxLength={30}
+                //  value={values.name}
+              />
+            </Form.Group>
+            <Form.Group className="pb-3">
+              <Form.Control
+                type="text"
+                placeholder="Ingrese su correo electrónico."
+                id="email"
+                maxLength={35}
+                //  value={values.name}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                as="textarea"
+                row={3}
+                placeholder="Ingrese su mensaje."
+                id="message"
+                maxLength={300}
+                //  value={values.name}
+              />
+            </Form.Group>
+            <button className="btn-send-contact">Enviar</button>
+          </Form>
         </Col>
-        <Col></Col>
+        <Col
+          md={6}
+          className="ps-3"
+          style={{ border: "2px solid black", height:'13rem' }}
+        ></Col>
       </Row>
     </Container>
   );
-}
+};
 
-export default Contact
+export default Contact;
