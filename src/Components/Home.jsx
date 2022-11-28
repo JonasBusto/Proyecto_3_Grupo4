84e835cdbc66cf4f445c830313029860087ecc21
Jonas Busto <jonasrex00@gmail.com>
2022-11-28 04:00:31 -0300

    corrigiendo errores de maquetado

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,14 @@ import content from "../arrayContent";
 import "../Styles/home.css";
 import "react-animated-slider/build/horizontal.css";
 import "../Styles/slider-animations.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMountain,
+  faTree,
+  faUmbrellaBeach,
+  faWater,
+} from "@fortawesome/free-solid-svg-icons";
+import provinceArray from "../arrayProvinces";
 
 const Home = () => {
   if (localStorage.getItem("Lugares") === null) {
@@ -52,7 +60,7 @@ const Home = () => {
           </Slider>
         </div>
 
-        <div className="div-category-advertising">
+        {/* <div className="div-category-advertising">
           <div className="row m-0">
             <div className="col-12 col-md-6 div-categoria">
               <h1 className="text-center">Categorias</h1>
@@ -121,9 +129,123 @@ const Home = () => {
               />
             </div>
           </div>
+        </div> */}
+
+        <div className="mt-5">
+          <div className="row m-0">
+            <div className="col-12 col-md-6">
+              <h1 className="text-center mb-2">CATEGORIAS</h1>
+              <div className="row m-0">
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faMountain} />
+                  </Link>
+                  <p>MONTAÑA</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faTree} />
+                  </Link>
+                  <p>SELVA</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faUmbrellaBeach} />
+                  </Link>
+                  <p>PLAYA</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faWater} />
+                  </Link>
+                  <p>CATARATAS</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faMountain} />
+                  </Link>
+                  <p>FESTIVALES</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faTree} />
+                  </Link>
+                  <p>SELVA</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faUmbrellaBeach} />
+                  </Link>
+                  <p>PLAYA</p>
+                </div>
+                <div className="col-3 col-icon-category d-flex flex-column align-items-center">
+                  <Link to="/">
+                    <FontAwesomeIcon icon={faWater} />
+                  </Link>
+                  <p>CATARATAS</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 col-md-6 d-none d-md-flex div-advertising justify-content-center align-items-center">
+              <img
+                className="img-fluid"
+                src="https://media.rpctv.com/p/bd46d329195159a20a773a02e983e540/adjuntos/314/imagenes/017/976/0017976660/1200x675/smart/panini-1jpg.jpg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="div-provinces d-flex flex-column">
+        <div className="mt-5">
+          <p className="text-center fs-1">LUGARES CON MAS LIKES</p>
+          <div className="div-place-more-liked d-flex">
+            {arrayPlaces.map((l) => (
+              <Link
+                key={l.id}
+                to={`/lugar/${l.id}`}
+                className="div-img-more-liked"
+              >
+                <img src={l.img.img1} alt="" />
+                <h1 className="title-place-more-liked d-flex flex-column align-items-center">
+                  {l.lugar.toUpperCase()}
+                  <b>{l.provincia.toUpperCase()}</b>
+                </h1>
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <p className="text-center fs-1">SOBRE NOSOTROS</p>
+          <div className="div-about-us-home d-flex flex-column align-items-center justify-content-center">
+            <p className="text-center text-about-us fs-1">GRUPO 4.JSX</p>
+            <p className="text-about-us2">
+              Somos un grupo que le gusta trabajar en equipo y le apasiciona la
+              programación, buscando el mejor resultado para todos.
+            </p>
+            <Link to="/nosotros">CONOCENOS</Link>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <p className="text-center fs-1">PROVINCIAS</p>
+          <div className="div-place-more-liked d-flex">
+            {provinceArray.map((p) => (
+              <Link
+                key={p.idProvince}
+                to={`/lugares/${p.tipo}/todas`}
+                className="div-img-more-liked"
+              >
+                <img src={p.img} alt="img_province" />
+                <h1 className="title-place-more-liked d-flex flex-column align-items-center">
+                  {p.nombre.toUpperCase()}
+                </h1>
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        {/* <div className="div-provinces d-flex flex-column">
           <p className="text-center fs-1">Provincias mas visitadas</p>
 
           <div className="w-100 div-sm-provinces-none">
@@ -318,7 +440,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* <div className="div-seeMore d-flex justify-content-center">
           <Link to="/provincias">VER MAS</Link>
